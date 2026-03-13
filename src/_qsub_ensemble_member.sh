38d123b2dfd3a131adc30f1d724c11019823b3c3
cbur24 <c.a.burton123@gmail.com>
2026-03-13 12:52:40 +1100

    updates for 2025

--- src/_qsub_ensemble_member.sh
+++ src/_qsub_ensemble_member.sh
@@ -2,14 +2,13 @@
  
 #PBS -P er8
 #PBS -q normalsr
-#PBS -l ncpus=52
-#PBS -l mem=400gb
-#PBS -l walltime=06:00:00
+#PBS -l ncpus=26
+#PBS -l mem=124gb
+#PBS -l walltime=01:00:00
 #PBS -l jobfs=1500MB
 #PBS -l storage=gdata/os22+gdata/xc0+scratch/xc0
  
 module load python3/3.10.0
 source /g/data/xc0/project/AusEFlux/env/py310/bin/activate
-# source /g/data/os22/chad_tmp/AusENDVI/env/py310/bin/activate
 
 python3 /g/data/xc0/project/AusEFlux/src/_batch_run_ensemble.py
